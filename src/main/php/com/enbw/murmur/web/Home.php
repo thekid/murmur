05e2a426b1a675cfc6de51eafba09f7d35ccf0dc
Timm Friebe <thekid@thekid.de>
2020-12-27 14:46:25 +0100

    Cache groups

--- src/main/php/com/enbw/murmur/web/Home.php
+++ src/main/php/com/enbw/murmur/web/Home.php
@@ -5,15 +5,23 @@ use web\frontend\{Handler, Get, Value};
 
 #[Handler('/')]
 class Home {
+  private static $groups= [];
 
   public function __construct(private YammerAPI $yammer) { }
 
   #[Get]
   public function index(#[Value] $user) {
     $api= $this->yammer->as($user['token']);
-    return [
-      'user'   => $user['identity'],
-      'groups' => $api->resource('groups/for_user/{id}.json', $user['identity'])->get()->value()
-    ];
+
+    // Cache groups
+    $id= $user['identity']['id'];
+    if (!isset(self::$groups[$id])) {
+      while (sizeof(self::$groups) > 100) {
+        unset(self::$groups[key(self::$groups)]);
+      }
+      self::$groups[$id]= $api->resource('groups/for_user/{id}.json', $user['identity'])->get()->value();
+    }
+
+    return ['user' => $user['identity'], 'groups' => self::$groups[$id]];
   }
 }
