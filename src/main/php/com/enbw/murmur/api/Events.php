c3821164ccb2ff76594bfd70d47d3936c160eaf9
Timm Friebe <thekid@thekid.de>
2020-12-30 00:24:55 +0100

    Include live events in group view

--- src/main/php/com/enbw/murmur/api/Events.php
+++ src/main/php/com/enbw/murmur/api/Events.php
@@ -1,6 +1,7 @@
 <?php namespace com\enbw\murmur\api;
 
 use com\enbw\murmur\YammerAPI;
+use util\Date;
 use web\rest\{Get, Resource, Param, Value};
 
 #[Resource('/api/events')]
@@ -13,6 +14,17 @@ class Events {
     return $this->yammer->as($user['token'])
       ->query('NetworkLiveEventsClients', hash: 'd0e0c0055f8df84dbd9fb54b8f03c8cf6efc0e9175c8a967ca344e422fa2608f')
       ->execute(['first' => $limit])
+    ;
+  }
+
+  #[Get('/group/{id}')]
+  public function group(#[Value] $user, string $id, #[Param] string $start= null) {
+    return $this->yammer->as($user['token'])
+      ->query('GroupLiveEventsClients', hash: '698feacf237f30b87f7ff1caf4b1133a9385607e6e8704ccc7b8035801c45149')
+      ->execute([
+        'groupId'    => $this->yammer->id('Group', $id),
+        'startAfter' => new Date($start)->toString('c')
+      ])
     ;
   }
 }
