c9dba064fcdfe2c4e56338de9ecd6def3d636fee
Timm Friebe <thekid@thekid.de>
2020-12-29 22:01:17 +0100

    Simplify APIs

--- src/main/php/com/enbw/murmur/api/Feed.php
+++ src/main/php/com/enbw/murmur/api/Feed.php
@@ -5,31 +5,19 @@ use web\rest\{Get, Resource, Param, Value};
 
 #[Resource('/api/feed')]
 class Feed {
-  private const HASH = '7807875433b9a6621e71bbe5937236b7464feef1dbcf56b415d93fa3e68a44a7';
+  private const FEEDS = [
+    'all'       => 'ALL_PUBLIC_GROUPS',
+    'discovery' => 'DISCOVERY',
+    'following' => 'FOLLOWING_AND_ALLCOMPANY'
+  ];
 
   public function __construct(private YammerAPI $yammer) { }
 
-  #[Get('/all')]
-  public function all(#[Value] $user, #[Param] int $limit= 10) {
+  #[Get('/{kind}')]
+  public function all(#[Value] $user, string $kind, #[Param] int $limit= 10) {
     return $this->yammer->as($user['token'])
-      ->query('FeedHomeClients', self::HASH)
-      ->execute(['feedType' => 'ALL_PUBLIC_GROUPS', 'threadCount' => $limit, 'replyCount' => 0])
-    ;
-  }
-
-  #[Get('/discovery')]
-  public function discovery(#[Value] $user, #[Param] int $limit= 10) {
-    return $this->yammer->as($user['token'])
-      ->query('FeedHomeClients', self::HASH)
-      ->execute(['feedType' => 'DISCOVERY', 'threadCount' => $limit, 'replyCount' => 0])
-    ;
-  }
-
-  #[Get('/following')]
-  public function following(#[Value] $user, #[Param] int $limit= 10) {
-    return $this->yammer->as($user['token'])
-      ->query('FeedHomeClients', self::HASH)
-      ->execute(['feedType' => 'FOLLOWING_AND_ALLCOMPANY', 'threadCount' => $limit, 'replyCount' => 0])
+      ->query('FeedHomeClients', '7807875433b9a6621e71bbe5937236b7464feef1dbcf56b415d93fa3e68a44a7')
+      ->execute(['feedType' => self::FEEDS[$kind], 'threadCount' => $limit, 'replyCount' => 0])
     ;
   }
 }
--- src/main/php/com/enbw/murmur/api/Groups.php
+++ src/main/php/com/enbw/murmur/api/Groups.php
@@ -5,32 +5,22 @@ use web\rest\{Get, Resource, Param, Value};
 
 #[Resource('/api/grouped')]
 class Groups {
-  private const HASH = '0079bf67919fe5d57572a9144e3eb6efc3354a7da8713a68c1e804061f928fed';
+  private const FEEDS = [
+    'all'       => 'ALL',
+    'questions' => 'QUESTIONS',
+  ];
 
   public function __construct(private YammerAPI $yammer) { }
 
-  #[Get('/all/{id}')]
-  public function all(#[Value] $user, $id, #[Param] int $limit= 10) {
+  #[Get('/{kind}/{id}')]
+  public function all(#[Value] $user, string $kind, string $id, #[Param] int $limit= 10) {
     return $this->yammer->as($user['token'])
-      ->query('FeedGroupClients', self::HASH)
+      ->query('FeedGroupClients', '0079bf67919fe5d57572a9144e3eb6efc3354a7da8713a68c1e804061f928fed')
       ->execute([
         'groupId'       => base64_encode(json_encode(['_type' => 'Group', 'id' => $id])),
         'threadCount'   => $limit,
         'replyCount'    => 0,
-        'groupFeedType' => 'ALL'
-      ])
-    ;
-  }
-
-  #[Get('/questions/{id}')]
-  public function questions(#[Value] $user, $id, #[Param] int $limit= 10) {
-    return $this->yammer->as($user['token'])
-      ->query('FeedGroupClients', self::HASH)
-      ->execute([
-        'groupId'       => base64_encode(json_encode(['_type' => 'Group', 'id' => $id])),
-        'threadCount'   => $limit,
-        'replyCount'    => 0,
-        'groupFeedType' => 'QUESTIONS'
+        'groupFeedType' => self::FEEDS[$kind]
       ])
     ;
   }
