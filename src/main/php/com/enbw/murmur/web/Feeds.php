e72668f91a13665b195c7912e16ae120558c2a97
Timm Friebe <thekid@thekid.de>
2020-12-31 23:49:56 +0100

    Inside feeds, mark followed groups

--- src/main/php/com/enbw/murmur/web/Feeds.php
+++ src/main/php/com/enbw/murmur/web/Feeds.php
@@ -1,12 +1,27 @@
 <?php namespace com\enbw\murmur\web;
 
+use com\enbw\murmur\{YammerAPI, Cache};
 use web\frontend\{Handler, Get, Value, View};
 
 #[Handler('/feed')]
 class Feeds {
 
+  public function __construct(private YammerAPI $yammer, private Cache $cache) { }
+
   #[Get('/{kind}')]
   public function all(#[Value] $user) {
-    return View::named('feeds');
+
+    // Populate group IDs
+    $groups= $this->cache->lookup($user['identity']['id'], 'groups', fn() => $this->yammer->as($user['token'])
+      ->api('groups/for_user/{id}', $user['identity'])
+      ->get()
+      ->value()
+    );
+    $map= [];
+    foreach ($groups as $group) {
+      $map[$group['id']]= true;
+    }
+
+    return View::named('feeds')->with(['following' => json_encode($map)]);
   }
 }
