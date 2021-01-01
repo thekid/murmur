682c61a099e629b0447acb01ea7d982f12bbd755
Timm Friebe <thekid@thekid.de>
2021-01-01 12:56:05 +0100

    Remove unused class

--- src/main/php/com/enbw/murmur/yammer/Endpoint.php
+++
@@ -1,9 +0,0 @@
-<?php namespace com\enbw\murmur\yammer;
-
-use util\Secret;
-use webservices\rest\Endpoint as RestEndpoint;
-
-class Endpoint {
-
-  public function __construct(private Secret $token) 
-}
