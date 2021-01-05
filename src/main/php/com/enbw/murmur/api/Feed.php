4307d0e6ad73742dad808088f981e62039ea90b8
Timm Friebe <thekid@thekid.de>
2021-01-05 10:03:41 +0100

    QA: Rename lookup map to match Feed[X]Clients

--- src/main/php/com/enbw/murmur/api/Feed.php
+++ src/main/php/com/enbw/murmur/api/Feed.php
@@ -5,7 +5,7 @@ use web\rest\{Get, Resource, Param, Value};
 
 #[Resource('/api/feed')]
 class Feed {
-  private const FEED_TYPES = [
+  private const HOME_TYPES = [
     'all'       => 'ALL_PUBLIC_GROUPS',
     'discovery' => 'DISCOVERY',
     'following' => 'FOLLOWING_AND_ALLCOMPANY',
@@ -21,7 +21,7 @@ class Feed {
   public function all(#[Value] $user, string $type, #[Param] int $limit= 10) {
     return $this->yammer->as($user['token'])
       ->query('FeedHomeClients', '7807875433b9a6621e71bbe5937236b7464feef1dbcf56b415d93fa3e68a44a7')
-      ->execute(['feedType' => self::FEED_TYPES[$type], 'threadCount' => $limit, 'replyCount' => 0])
+      ->execute(['feedType' => self::HOME_TYPES[$type], 'threadCount' => $limit, 'replyCount' => 0])
     ;
   }
 
