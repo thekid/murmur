9c895285c09c52a91131357ea168f50f7ad23c0c
Timm Friebe <thekid@thekid.de>
2021-01-08 16:56:06 +0100

    Add autocompletion on groups and users

--- src/main/php/com/enbw/murmur/api/Autocomplete.php
+++ src/main/php/com/enbw/murmur/api/Autocomplete.php
@@ -5,15 +5,54 @@ use web\rest\{Get, Resource, Param, Value};
 
 #[Resource('/api/autocomplete')]
 class Autocomplete {
+  private const HASH = '0055d09b4b7f917e812e9947d84227b1dd209f2cad8e39fa61b860affc2e1509';
 
   public function __construct(private YammerAPI $yammer) { }
+
+  #[Get]
+  public function all(#[Value] $user, #[Param] string $q) {
+    if (strlen($q) < 1) return [];
+
+    $result= $this->yammer->as($user['token'])
+      ->query('AutocompleteClients', self::HASH)
+      ->execute([
+        'text'          => $q,
+        'countEach'     => 10,
+        'includeGroups' => true,
+        'includeUsers'  => true,
+        'includeTopics' => false,
+      ])
+    ;
+
+    $r= [
+      'results' => [
+        'users'  => ['name' => 'Users', 'results' => []],
+        'groups' => ['name' => 'Groups', 'results' => []],
+      ]
+    ];
+    foreach ($result['autocompleteSuggestions']['users']['edges'] as $edge) {
+      $r['results']['users']['results'][]= [
+        'title'       => $edge['node']['displayName'],
+        'url'         => '/user/'.$edge['node']['databaseId'],
+        'description' => $edge['node']['email'],
+      ];
+    }
+    foreach ($result['autocompleteSuggestions']['groups']['edges'] as $edge) {
+      $r['results']['groups']['results'][]= [
+        'title'       => $edge['node']['displayName'],
+        'url'         => '/group/'.$edge['node']['databaseId'],
+        'description' => $edge['node']['description'],
+      ];
+    }
+    return $r;
+  }
 
   #[Get('/topics')]
   public function topics(#[Value] $user, #[Param] string $q) {
     if (strlen($q) < 1) return [];
 
     $result= $this->yammer->as($user['token'])
-      ->query('AutocompleteClients', '0055d09b4b7f917e812e9947d84227b1dd209f2cad8e39fa61b860affc2e1509')
+      ->query('AutocompleteClients', self::HASH)
       ->execute([
         'text'          => $q,
         'includeGroups' => false,
